e4488241857aded96be2e11f847a6c65d8a00efa
AhireVaibhav <132124238+AhireVaibhav@users.noreply.github.com>
2024-07-07 19:28:17 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,120 @@
+import streamlit as st
+import os
+from PyPDF2 import PdfReader
+import docx
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from streamlit_chat import message
+from dotenv import load_dotenv
+from sentence_transformers import SentenceTransformer
+
+# Load environment variables
+load_dotenv()
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+# "with" notation
+def main():
+    st.set_page_config(page_title="Chat with your file")
+    st.header("DocumentGPT")
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+    if "processComplete" not in st.session_state:
+        st.session_state.processComplete = None
+
+    with st.sidebar:
+        uploaded_files = st.file_uploader("Upload your file", type=['pdf', 'docx'], accept_multiple_files=True)
+        process = st.button("Process")
+
+    if process:
+        if not openai_api_key:
+            st.info("Please add your OpenAI API key to continue.")
+            st.stop()
+        files_text = get_files_text(uploaded_files)
+        st.write("File loaded...")
+        text_chunks = get_text_chunks(files_text)
+        st.write("File chunks created...")
+        vectorstore = get_vectorstore(text_chunks)
+        st.write("Vector Store Created...")
+        st.session_state.conversation = get_conversation_chain(vectorstore, openai_api_key)
+        st.session_state.processComplete = True
+
+    if st.session_state.processComplete:
+        user_question = st.text_input("Ask a question about your files.")
+        if user_question:
+            handle_user_input(user_question)
+
+def get_files_text(uploaded_files):
+    text = ""
+    for uploaded_file in uploaded_files:
+        split_tup = os.path.splitext(uploaded_file.name)
+        file_extension = split_tup[1].lower()
+        if file_extension == ".pdf":
+            text += get_pdf_text(uploaded_file)
+        elif file_extension == ".docx":
+            text += get_docx_text(uploaded_file)
+    return text
+
+def get_pdf_text(pdf):
+    pdf_reader = PdfReader(pdf)
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+    return text
+
+def get_docx_text(file):
+    doc = docx.Document(file)
+    all_text = []
+    for docpara in doc.paragraphs:
+        all_text.append(docpara.text)
+    return ' '.join(all_text)
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=900,
+        chunk_overlap=100,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vectorstore(text_chunks):
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    knowledge_base = FAISS.from_texts(text_chunks, embeddings)
+    return knowledge_base
+
+def get_conversation_chain(vectorstore, openai_api_key):
+    llm = ChatOpenAI(openai_api_key=openai_api_key,
+                     model_name='gpt-3.5-turbo', 
+                     temperature=0)
+    memory = ConversationBufferMemory(memory_key='chat_history',
+                                      return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+def handle_user_input(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    response_container = st.container()
+
+    with response_container:
+        for i, messages in enumerate(st.session_state.chat_history):
+            if i % 2 == 0:
+                message(messages.content, is_user=True, key=str(i))
+            else:
+                message(messages.content, key=str(i))
+
+if __name__ == '__main__':
+    main()
